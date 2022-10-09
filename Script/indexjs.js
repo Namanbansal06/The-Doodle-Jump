7c53ee2ac194d5073617728e2f18300274041536
naman <namanbansal34@gmail.co>
2022-10-10 00:49:30 +0530

    direction to ball GIVEN

--- Script/indexjs.js
+++ Script/indexjs.js
@@ -3,28 +3,14 @@ canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 const c = canvas.getContext('2d');
 
-var mouse={
-    x:undefined,
-    y:undefined
-};
-addEventListener("mousemove",function(event){
-    mouse.x=event.x;
-    mouse.y=event.y;
-})
-addEventListener("resize",function(){
-    canvas.width=this.innerWidth;
-    canvas.height=this.innerHeight;
-
-    init();
-});
-function randomIntFromRange(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-function object(x,y,dy,r){
+function object(x,y,dy,dx,r){
     this.x=x;
     this.y=y;
     this.dy=dy;
     this.r=r;
+    this.dx=dx;
+    var key=false;
+    var playerdir=0;
     this.update=function(){
         if(this.y+this.r>canvas.height){
             this.dy=-this.dy;
@@ -35,6 +21,48 @@ function object(x,y,dy,r){
         }
         this.y+=this.dy;
         this.draw();
+// key movements
+        window.addEventListener("keydown",(e)=>{
+            switch(e.key){
+                case 'ArrowRight':
+                    key=true;
+                    this.dx=8;
+                    playerdir=1;
+                    break;
+                    case 'ArrowLeft':
+                        key=true;
+                        this.dx=-8;
+                        playerdir=-1;
+                        break;
+                        default:
+                            break;
+            }
+        });
+        document.addEventListener('keyup', function(e) {
+            key = false;
+          });
+
+          if (!key) {
+            if (playerDir < 0) {
+              if (this.x > 0) {
+                this.x = 0;
+                playerDir = 0;
+              }
+            }
+            else if (playerDir > 0) {
+              if (this.dx < 0) {
+                this.dx = 0;
+                playerDir = 0;
+              }
+            }
+          }
+          if(this.x+this.r>canvas.width){
+            this.dx=-this.dx;
+          }
+          else if(this.x-this.r<0){
+            this.dx=-this.dx;
+          }
+          this.x += this.dx;
     }
     this.draw=function(){
         c.beginPath();
@@ -47,10 +75,9 @@ function object(x,y,dy,r){
 var ball;
 function init(){
     //console.log("naman1");
-    ball=new object(canvas.width/2,canvas.height/2,2,30);
+    ball=new object(canvas.width/2,canvas.height/2,2,1,30);
     //console.log("naman2");
 }
-
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height);
