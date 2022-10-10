c3be25dc176cecde1db994c7821d3d93e4eeee84
naman <namanbansal34@gmail.co>
2022-10-10 17:23:43 +0530

    merging

--- Script/indexjs.js
+++ Script/indexjs.js
@@ -3,7 +3,7 @@ canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 const c = canvas.getContext('2d');
 
-function object(x,y,dy,dx,r){
+function Ball(x,y,dy,dx,r){
     this.x=x;
     this.y=y;
     this.dy=dy;
@@ -17,7 +17,6 @@ function object(x,y,dy,dx,r){
         }
         else{
             this.dy+=1;
-            //console.log(this.dy);
         }
         this.y+=this.dy;
         this.draw();
@@ -65,23 +64,45 @@ function object(x,y,dy,dx,r){
           this.x += this.dx;
     }
     this.draw=function(){
-        c.beginPath();
-        c.arc(this.x,this.y,this.r,0,Math.PI*2,0);
-        c.fillStyle='red';
-        c.fill();
-        c.closePath();
+      c.beginPath();
+      c.fillStyle='red';
+      c.arc(this.x,this.y,this.r,0,Math.PI*2,0);
+      c.fill();
+      c.closePath();
     };
 }
 var ball;
 function init(){
-    //console.log("naman1");
-    ball=new object(canvas.width/2,canvas.height/2,2,1,30);
-    //console.log("naman2");
+    ball=new Ball(canvas.width/2,canvas.height/2,2,1,30);
 }
+
+// //Bars
+// var bar_height;
+// var bar_width;
+// var bar_x;
+// var bar_y;
+// function Bar(bar_x,bar_y,bar_width,bar_height){
+//   update1=function(){
+//     draw1();
+//   }
+//   draw1=function(){
+//     c.beginPath();
+//       c.fillStyle='green';
+//       c.fillRect(bar_x,bar_y,bar_width,bar_height);
+//       c.fill();
+//       c.closePath();
+//   };
+// }
+// var bar;
+// function init2(){
+//   bar=new Bar(canvas.width/2,500,100,100)
+// }
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height);
     ball.update();
+    //bar.update1();
 }
 init();
+init2();
 animate();
