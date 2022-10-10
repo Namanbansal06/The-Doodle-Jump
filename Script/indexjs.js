82d99a9578884739e7457cfe7f7d92e6f006e3b5
naman <namanbansal34@gmail.co>
2022-10-10 17:37:20 +0530

    ball bug fixes

--- Script/indexjs.js
+++ Script/indexjs.js
@@ -76,27 +76,27 @@ function init(){
     ball=new Ball(canvas.width/2,canvas.height/2,2,1,30);
 }
 
-// //Bars
-// var bar_height;
-// var bar_width;
-// var bar_x;
-// var bar_y;
-// function Bar(bar_x,bar_y,bar_width,bar_height){
-//   update1=function(){
-//     draw1();
-//   }
-//   draw1=function(){
-//     c.beginPath();
-//       c.fillStyle='green';
-//       c.fillRect(bar_x,bar_y,bar_width,bar_height);
-//       c.fill();
-//       c.closePath();
-//   };
-// }
-// var bar;
-// function init2(){
-//   bar=new Bar(canvas.width/2,500,100,100)
-// }
+//Bars
+var bar_height;
+var bar_width;
+var bar_x;
+var bar_y;
+function Bar(bar_x,bar_y,bar_width,bar_height){
+  update1=function(){
+    draw1();
+  }
+  draw1=function(){
+    c.beginPath();
+      c.fillStyle='green';
+      c.fillRect(bar_x,bar_y,bar_width,bar_height);
+      c.fill();
+      c.closePath();
+  };
+}
+var bar;
+function init2(){
+  bar=new Bar(canvas.width/2,500,100,100)
+}
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height);
@@ -104,5 +104,5 @@ function animate(){
     //bar.update1();
 }
 init();
-init2();
 animate();
+//init2();
