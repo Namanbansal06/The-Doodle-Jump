86ac04af0b3a002e004d5308b62bfab48513b36f
naman <namanbansal34@gmail.co>
2022-10-11 17:13:51 +0530

    merging to the master

--- Script/indexjs.js
+++ Script/indexjs.js
@@ -1,138 +1,226 @@
-const canvas = document.getElementById('game');
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
-const c = canvas.getContext('2d');
+window.addEventListener('load', () => {
+  const canvas = document.getElementById('GCanvas');
+  const ctx = canvas.getContext('2d');
+  const CANVAS_WIDTH = canvas.width = window.innerWidth;
+  const CANVAS_HEIGHT = canvas.height = window.innerHeight;
+  class Background{
+      constructor(game){
+          this.game = game;
+          this.x = 0;
+          this.y = 0;
+          this.width =1278;
+          this.height = 680;
+          this.image = document.getElementById('bg1');
+      }
+      update(){
+          if(this.y > this.height){
+              this.y = 0;
+              this.game.add_platforms(-this.height*2, -15);
+          }
+          else{
+              this.y += 2;
+          }
+      }
+      draw(){
+          ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+          ctx.drawImage(this.image, this.x, this.y-this.height, this.width, this.height);
+      }
+  }
+  
+  class Platform{
+      constructor(game, lowerY, upperY, type){
+          this.game = game;
+          this.width = 121;
+          this.height = 35;
+          this.type = type;
+          this.x = Math.floor(Math.random()*((this.game.width-this.width) + 1));
+          this.y =  this.calc_Y(upperY, lowerY);
+          this.vx = (this.type == 'blue') ? this.game.object_vx : 0;
+          this.image = document.getElementById('tiles');
+          this.markedForDeletion = false;
+      }
+  
+      update(){
+          if(this.type == 'blue'){
+              if(this.x < 0 || this.x > this.game.width - this.width) this.vx *= -1;
+          }
+          this.x += this.vx;
+          this.y += 3;
+      }
+  
+      draw(context){
+          let k;
+          if(this.type == 'green'){
+              k=0;
+          }
+          else if(this.type == 'blue'){
+              k=35;
+          }
+          else{
+              k=70;
+          }
+          context.drawImage(this.image, 0, k, 121, 35, this.x, this.y, this.width, this.height);
+      }
+  
+      calc_Y(upperY, lowerY){
+          if(!this.game.platforms.length){
+              return Math.floor(Math.random() * (upperY - (upperY-100) + 1)) + (upperY-100);
+          }
+          else{
+              return this.game.platforms[0].y - (Math.floor(Math.random() * (this.game.platform_gap  - (this.game.platform_gap - 30) + 1)) + (this.game.platform_gap - 30));
+          }
+      }
+  }
 
-function Ball(x,y,dy,dx,r){
-    this.x=x;
-    this.y=y;
-    this.dy=dy;
-    this.r=r;
-    this.dx=dx;
-    var key=false;
-    var playerdir=0;
-    const dd=0.3;
-    this.update=function(){
-        if(this.x<canvas.width/2-200+400 &&
-        this.x+this.r>canvas.width/2-200&&
-        this.y<500+40&&
-        this.y+this.r>500
-        ){
-            this.dy=-(this.dy);
-        }
-        else if(this.y+this.r>canvas.height){
-          this.dy=-this.dy;
-        }
-        else{
-            this.dy+=1;
-        }
-        this.y+=this.dy;
-        this.draw();
-// key movements
-        window.addEventListener("keydown",(e)=>{
-            switch(e.key){
-                case 'ArrowRight':
-                    key=true;
-                    this.dx=8;
-                    playerdir=1;
-                    break;
-                    case 'ArrowLeft':
-                        key=true;
-                        this.dx=-8;
-                        playerdir=-1;
-                        break;
-                        default:
-                            break;
-            }
-        });
-        document.addEventListener('keyup', function(e) {
-            key = false;
+  class Player{
+      constructor(game){
+          this.game = game;
+          this.sizeModifier = 0.2;
+          this.width = 395 * this.sizeModifier;
+          this.height = 488 * this.sizeModifier;
+          this.x = this.game.platforms.filter(platform => platform.type == 'green').slice(-1)[0].x +6;
+          this.y = this.game.platforms.filter(platform => platform.type == 'green').slice(-1)[0].y - this.height;
+          this.min_y = (this.game.height/2)-30;
+          this.min_vy = -18;
+          this.max_vy = this.game.platforms[0].height;
+          this.vy = this.min_vy;
+          this.weight = 0.5;
+          this.image = document.getElementById('player1');
+          this.vx = 0;
+          this.max_vx = 8;
+      }
+
+      update(inputHandler){
+          this.x +=this.vx;
+          if(inputHandler.keys.includes('ArrowLeft')){
+              this.vx = -this.max_vx;
+          }
+          else if(inputHandler.keys.includes('ArrowRight')){
+              this.vx = this.max_vx;
+          }
+          else{
+              this.vx = 0;
+          }
+
+          if(this.x < -this.width/2){
+              this.x = this.game.width - (this.width/2);
+          }
+          if(this.x + (this.width/2) > this.game.width) {
+              this.x = (-this.width/2);
+          }
+          if(this.y+this.height/2>canvas.height){
+              this.vy=-this.vy;
+          }
+          else{
+              this.vy+=1;
+          }
+          this.y+=this.vy;
+      }
+
+      draw(context){
+          context.drawImage(this.image,this.x, this.y, this.width, this.height);
+      }
+
+      onPlatform(){
+          lettype = null;
+          letplayerHitBox = {
+              x:this.x+15,
+              y:this.y,
+              width:this.width-30,
+              height:this.height
+          }
+      }
+  }
+
+  class InputHandler{
+      constructor(game){
+          this.keys = [];
+          this.game = game;
+          
+          window.addEventListener('keydown', (e) =>{
+              if((e.key == 'ArrowLeft' || e.key == 'ArrowRight') && !this.keys.includes(e.key)){
+                  this.keys.push(e.key)
+              }
+              if(e.key == 'Enter'){
+                  this.game.gameStart = true;
+              }
           });
 
-          if (!key) {
-            if (playerdir < 0) {
-              this.dx+=dd;
-              if (this.dx > 0) {
-                this.dx = 0;
-                playerdir = 0;
+          window.addEventListener('keyup', (e) => {
+              if((e.key == 'ArrowLeft' || e.key == 'ArrowRight') && this.keys.includes(e.key)){
+                  this.keys.splice(this.keys.indexOf(e.key), 1);
               }
-            }
-            else if (playerdir > 0) {
-              this.dx-=dd;
-              if (this.dx < 0) {
-                this.dx = 0;
-                playerdir = 0;
-              }
-            }
-          }
-          if(this.x+this.r>canvas.width){
-            this.dx=-this.dx;
-          }
-          else if(this.x-this.r<0){
-            this.dx=-this.dx;
-          }
-          this.x += this.dx;
-    }
-    this.draw=function(){
-      c.beginPath();
-      c.fillStyle='red';
-      c.arc(this.x,this.y,this.r,0,Math.PI*2,0);
-      c.fill();
-      c.closePath();
-
-      // c.beginPath();
-      // c.fillStyle='green';
-      // c.fillRect(canvas.width/2-200,400,400,40);
-      // c.fill();
-      // c.closePath();
-    };
-}
-var ball;
-function init(){
-    ball=new Ball(canvas.width/2,canvas.height/2,2,1,30);
-}
-
-//Bars
-var bar_height;
-var bar_width;
-var bar_x;
-var bar_y;
-function Bar(bar_x,bar_y,bar_width,bar_height){
-  this.bar_x=bar_x;
-  this.bar_y=bar_y;
-  this.bar_width=bar_width;
-  this.bar_height=bar_height;
-  this.update1=function(){
-    this.draw1();
-    //console.log("naman");
+          });
+      }
   }
-  this.draw1=function(){
-    c.beginPath();
-      c.fillStyle='green';
-      c.fillRect(this.bar_x,this.bar_y,this.bar_width,this.bar_height);
-      c.fill();
-      c.closePath();
-  };
-}
-var bar;
-function init2(){
-  bar=new Bar(canvas.width/2-200,500,400,40)
-}
+  
+  class Game{
+      constructor(width, height){
+          this.width = width;
+          this.height  = height;
+          this.vy = 0;
+          this.gameStart = false;
+          this.platforms = [];
+          this.object_vx = 3;
+          this.platform_gap = 65;
+          this.blue_white_platform_chance = 20;
+          this.add_platforms(0, this.height-15);
+          this.add_platforms(-this.height, -15);
+          this.background = new Background(this); 
+          this.player = new Player(this);
+          this.inputHandler = new InputHandler(this);
+          
+      }
+  
+      update(){
+          this.background.update();
 
-function collision(){
-  if(this.x<this.bar_x+this.bar_width &&
-    this.x+this.r>this.bar_x&&
-    this.y<this.bar_y+this.height&&
-    this.y+this.r>this.bar_y){
-      this.dy=-this.dy;
-    }
-}
-function animate(){
-    requestAnimationFrame(animate);
-    c.clearRect(0,0,canvas.width,canvas.height);
-    ball.update();
-    bar.update1();
-}
-init();
-init2();
-animate();
+          this.platforms.forEach(platform =>{
+              platform.update();
+          });
+
+          this.player.update(this.inputHandler);
+
+          this.platforms = this.platforms.filter(platform => !platform.markedForDeletion);
+      }
+  
+      draw(context){
+          this.background.draw(context);
+
+          if(!this.gameStart){
+              context.font = "bold 30px Helvetica";
+              context.fillStyle = "black";
+              context.textAlign = "center";
+              context.fillText("Press Enter to Start", this.width*0.5, this.height*0.5);
+          }
+          else{
+              this.platforms.forEach(platform => {
+                  platform.draw(context);
+              });
+
+              this.player.draw(context);
+          }
+      }
+
+      add_platforms(lowerY, upperY){
+          do{
+              let type = 'green';
+              if(Math.random() < (this.blue_white_platform_chance/100)){
+                  type = (Math.random() < 0.5) ? 'blue' : 'white';
+              }
+              this.platforms.unshift(new Platform(this, lowerY, upperY, type));
+          }while(this.platforms[0].y >= lowerY);
+      }
+      
+  }
+  
+  const game = new Game(CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  function animate(){
+      ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      if(game.gameStart) game.update();
+      game.draw(ctx);
+      requestAnimationFrame(animate);
+  }
+  animate();
+});
